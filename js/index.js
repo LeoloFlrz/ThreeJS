f47c6276d2ff2ffe05382ddb85bfe30e9b28b8d9
LeoloFlrz <leoloflrzr@gmail.com>
2023-08-19 14:55:26 +0200

    added some geometry and orbit controls working

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -6,19 +7,40 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+const controls = new OrbitControls( camera, renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
+const cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+const cubeMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
 scene.add( cube );
 
-camera.position.z = 5;
+const cylinderGeometry = new THREE.CylinderGeometry( 1, 1, 2 );
+const cylinderMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff } )
+const cylinder = new THREE.Mesh( cylinderGeometry, cylinderMaterial );
+scene.add(cylinder);
+
+const coneGeometry = new THREE.ConeGeometry( 1, 2, 5 ); 
+const coneMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+const cone = new THREE.Mesh(coneGeometry, coneMaterial ); 
+scene.add( cone );
+
+cube.position.x = 3
+cylinder.position.x = 1
+cone.position.x = -1
+camera.position.set( 0, 5, 5 );
+controls.update();	
 
 function animate() {
 	requestAnimationFrame( animate );
 
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+
+	cylinder.rotation.x += 0.01;
+
+	cone.rotation.y -= 0.01;
+
+	controls.update();
 
 	renderer.render( scene, camera );
 }
